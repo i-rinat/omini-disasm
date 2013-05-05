75c6ad438b2a12b98cb66e6011d78395b4b5f903
Rinat <ibragimovrinat@mail.ru>
2013-05-05 17:06:24 +0400

    argh

--- entry.c
+++ entry.c
@@ -11,8 +11,14 @@ int main(void)
     uint32_t *text = read_section(fp, 0x12e8, 0x2df0);
     fclose(fp);
 
-    process_instruction(text[(0x2170 - 0x12e8)/4]);
-    process_instruction(text[(0x2170 - 0x12e8)/4 + 1]);
+    uint32_t ip = (0x2170 - 0x12e8)/4;
+    process_instruction(text[ip]);
+    process_instruction(text[ip + 1]);
+    process_instruction(text[ip + 2]);
+    process_instruction(text[ip + 3]);
+    process_instruction(text[ip + 4]);
+    process_instruction(text[ip + 5]);
+    process_instruction(text[ip + 6]);
 
     free(text);
     return 0;
