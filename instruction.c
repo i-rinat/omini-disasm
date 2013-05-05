49980f1450cb589394549a04231d4c08b6ee71e9
Rinat <ibragimovrinat@mail.ru>
2013-05-05 17:06:38 +0400

    more arghs

--- instruction.c
+++ instruction.c
@@ -20,8 +20,9 @@ arm_expand_imm12(uint32_t imm12)
 
 
 void
-p_push(uint32_t register_list)
+p_push(uint32_t code)
 {
+    const uint32_t register_list = code & 0xffff;
     uint32_t mask = 1;
     for (int reg = 0; reg < 16; reg ++) {
         if (register_list & mask)
@@ -30,7 +31,79 @@ p_push(uint32_t register_list)
     }
 }
 
+void
+p_sub_immediate(uint32_t code)
+{
+    const uint32_t imm12 = code & 0xfff;
+    const uint32_t imm32 = arm_expand_imm12(imm12);
 
+    const uint32_t Rd = (code >> 12) & 0xf;
+    const uint32_t Rn = (code >> 16) & 0xf;
+
+    emit_output("   r%d = r%d - %d;", Rd, Rn, imm32);
+    assert(Rd != 15);   // pc
+    int setflags = (code >> 20) & 1;
+    if (setflags) {
+        assert(0);
+    }
+}
+
+void
+p_sub_register(uint32_t code)
+{
+    assert(0);
+    const uint32_t imm5 = (code >> 7) & 0x1f;
+    const uint32_t type = (code >> 5) & 0x03;
+    const uint32_t Rd = (code >> 12) & 0x0f;
+    const uint32_t Rn = (code >> 16) & 0x0f;
+
+    assert(Rd != 15);
+    assert(Rd != 14);
+
+    int setflags = (code >> 20) & 1;
+    if (setflags) {
+        assert(0);
+    }
+}
+
+void
+p_sub_register_shifted_register(uint32_t code)
+{
+    assert(0);
+    int setflags = (code >> 20) & 1;
+    if (setflags) {
+        assert(0);
+    }
+}
+
+void
+p_str_immediate(uint32_t code)
+{
+    const uint32_t index = !!(code & (1 << 24));
+    const uint32_t add = !!(code & (1 << 23));
+    const uint32_t wback = !index || (code & (1 << 21));
+
+    const uint32_t Rn = (code >> 16) & 0xf;
+    const uint32_t Rt = (code >> 12) & 0xf;
+    const uint32_t imm12 = (code & 0xfff);
+    const int32_t imm32 = add ? imm12 : -imm12;
+
+    assert(Rt != 15);   // do not want stores into .text
+
+    if (index && !wback) {
+        emit_output("   store(r%d + %d, r%d);", Rn, imm32, Rt);
+    } else if (index && wback) {
+        emit_output("   r%d = r%d + %d;", Rn, Rn, imm32);
+        emit_output("   store(r%d, r%d);", Rn, Rt);
+    } else if (!index && wback) {
+        emit_output("   store(r%d, r%d);", Rn, Rt);
+        emit_output("   r%d = r%d + %d;", Rn, Rn, imm32);
+    } else if (!index && !wback) {
+        emit_output("   store(r%d, r%d);", Rn, Rt);
+    } else {
+        assert(0 && "logic error");
+    }
+}
 
 
 void
@@ -41,11 +114,17 @@ process_instruction(uint32_t code)
     if ((code & 0x0fff0fff) == 0x052d0004) {
         printf("push (single)\n");
         assert(0 && "not implemented");
-    } else
-    if ((code & 0x0fff0000) == 0x092d0000) {
-        p_push(code & 0xffff);
-    } else
-    {
-        assert(0 && "not implemented");
+    } else if ((code & 0x0fff0000) == 0x092d0000) {
+        p_push(code);
+    } else if ((code & 0x0fe00000) == 0x02400000) {
+        p_sub_immediate(code);
+    } else if ((code & 0x0fe00010) == 0x00400000) {
+        p_sub_register(code);
+    } else if ((code & 0x0fe00090) == 0x00400010) {
+        p_sub_register_shifted_register(code);
+    } else if ((code & 0x0e500000) == 0x04000000) {
+        p_str_immediate(code);
+    } else {
+        assert(0 && "instruction code not implemented");
     }
 }
