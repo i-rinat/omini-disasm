cb2e2c50a75a15d74a7f688dad92caadf71d3ffd
Rinat <ibragimovrinat@mail.ru>
2013-05-05 14:57:20 +0400

    arm_expand_imm12

--- instruction.c
+++ instruction.c
@@ -3,6 +3,22 @@
 #include <stdio.h>
 #include <assert.h>
 
+uint32_t
+arm_expand_imm12(uint32_t imm12)
+{
+    uint32_t rotation = (imm12 >> 8) & 0xf;
+    uint32_t abcdefgh = imm12 & 0xff;
+    switch (rotation) {
+    case 0: return abcdefgh;
+    case 1: return (abcdefgh >> 2) + ((abcdefgh & 0x03) << 30);
+    case 2: return (abcdefgh >> 4) + ((abcdefgh & 0x0f) << 28);
+    case 3: return (abcdefgh >> 6) + ((abcdefgh & 0x3f) << 26);
+    case 4: return                   ((abcdefgh & 0xff) << 24);
+    default: return abcdefgh << ((16 - rotation) * 2);
+    }
+}
+
+
 void
 p_push(uint32_t register_list)
 {
