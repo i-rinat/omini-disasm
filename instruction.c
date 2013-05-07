0513717de8c3ca853670d3f38c2f17fc387505b4
Rinat <ibragimovrinat@mail.ru>
2013-05-07 20:00:13 +0400

    simplify arm_expand_imm12

--- instruction.c
+++ instruction.c
@@ -39,16 +39,10 @@ end_function()
 uint32_t
 arm_expand_imm12(uint32_t imm12)
 {
-    uint32_t rotation = (imm12 >> 8) & 0xf;
-    uint32_t abcdefgh = imm12 & 0xff;
-    switch (rotation) {
-    case 0: return abcdefgh;
-    case 1: return (abcdefgh >> 2) + ((abcdefgh & 0x03) << 30);
-    case 2: return (abcdefgh >> 4) + ((abcdefgh & 0x0f) << 28);
-    case 3: return (abcdefgh >> 6) + ((abcdefgh & 0x3f) << 26);
-    case 4: return                   ((abcdefgh & 0xff) << 24);
-    default: return abcdefgh << ((16 - rotation) * 2);
-    }
+    uint32_t rotation = 2 * ((imm12 >> 8) & 0xf);
+    uint32_t imm8 = imm12 & 0xff;
+
+    return (imm8 >> rotation) | (imm8 << (32 - rotation));
 }
 
 enum SRType
