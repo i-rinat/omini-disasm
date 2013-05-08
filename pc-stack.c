f75ff1c30affc1a3f4ac5c51187a3af1ae4b5f47
Rinat <ibragimovrinat@mail.ru>
2013-05-08 17:40:46 +0400

    visited bitmap

--- pc-stack.c
+++ pc-stack.c
@@ -1,12 +1,15 @@
 #include "pc-stack.h"
 #include <assert.h>
 #include <stdlib.h>
+#include <glib.h>
 
 
 const uint32_t pc_stack_depth = 1000;
 
 uint32_t *pc_stack = NULL;
 uint32_t pc_stack_element_count = 0;
+
+GHashTable *visited_bitmap;
 
 void
 pc_stack_initialize()
@@ -42,4 +45,29 @@ pc_stack_free()
 {
     free(pc_stack);
     pc_stack = NULL;
+}
+
+void
+visited_bitmap_initialize()
+{
+    visited_bitmap = g_hash_table_new(g_direct_hash, g_direct_equal);
+    assert(visited_bitmap);
+}
+
+int
+visited_bitmap_visited(uint32_t pc)
+{
+    return GPOINTER_TO_INT(g_hash_table_lookup(visited_bitmap, GINT_TO_POINTER(pc)));
+}
+
+void
+visited_bitmap_mark_visited(uint32_t pc)
+{
+    g_hash_table_insert(visited_bitmap, GINT_TO_POINTER(pc), GINT_TO_POINTER(1));
+}
+
+void
+visited_bitmap_free()
+{
+    g_hash_table_destroy(visited_bitmap);
 }
--- pc-stack.h
+++ pc-stack.h
@@ -7,3 +7,8 @@ int         pc_stack_empty();
 uint32_t    pc_stack_pop();
 void        pc_stack_push(uint32_t);
 void        pc_stack_free();
+
+void        visited_bitmap_initialize();
+int         visited_bitmap_visited(uint32_t pc);
+void        visited_bitmap_mark_visited(uint32_t pc);
+void        visited_bitmap_free();
