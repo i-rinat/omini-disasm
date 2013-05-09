d219c2bcee5cdfa7df8d217d0fe1a465eaf40c0c
Rinat <ibragimovrinat@mail.ru>
2013-05-10 01:16:41 +0400

    p_and_immediate

--- instruction.c
+++ instruction.c
@@ -552,6 +552,32 @@ p_lsl_immediate(uint32_t pc, uint32_t code)
 }
 
 void
+p_and_immediate(uint32_t pc, uint32_t code)
+{
+    const uint32_t setflags = code & (1 << 20);
+    const uint32_t Rn = (code >> 16) & 0x0f;
+    const uint32_t Rd = (code >> 12) & 0x0f;
+    const uint32_t imm12 = code & 0xfff;
+    const uint32_t imm32 = arm_expand_imm12(imm12);
+
+    assert(Rd != 15);
+    if (15 == Rn) {
+        emit_code("    r%d = %d;", Rd, (pc + 8) & imm32);
+    } else {
+        emit_code("    r%d = r%d & %d;", Rd, Rn, imm32);
+    }
+
+    if (setflags) {
+        emit_code("    APSR.N = !!(r%d & 0x80000000);", Rd);
+        emit_code("    APSR.Z = (r%d == 0);", Rd);
+        emit_code("    APSR.C = %d;", !!(imm32 & 0x80000000));
+        // V unchanged
+    }
+
+    pc_stack_push(pc + 4);
+}
+
+void
 process_instruction(uint32_t pc)
 {
     uint32_t code = get_word_at(pc);
@@ -614,6 +640,8 @@ process_instruction(uint32_t pc)
         p_cmp_register(pc, code);
     } else if ((code & 0x0fe00070) == 0x01a00000) {
         p_lsl_immediate(pc, code);
+    } else if ((code & 0x0fe00000) == 0x02000000) {
+        p_and_immediate(pc, code);
     } else {
         assert(0 && "instruction code not implemented");
     }
