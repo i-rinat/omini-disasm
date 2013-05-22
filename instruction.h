2263546870ccb91b17919ca3e6c322b93a66aab1
Rinat <ibragimovrinat@mail.ru>
2013-05-22 15:13:18 +0400

    export arm_expand_imm12

--- instruction.h
+++ instruction.h
@@ -5,3 +5,6 @@ void        process_instruction(uint32_t pc);
 void        begin_function(uint32_t pc);
 int         function_end_found();
 void        end_function();
+
+// aux functions
+uint32_t    arm_expand_imm12(uint32_t imm12);
