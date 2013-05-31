d7589c98bcf5f632f9c716097cd5366a8bdb9380
Rinat <ibragimovrinat@mail.ru>
2013-06-01 00:56:55 +0400

    +p_and_register_shifted_register

--- instruction.c
+++ instruction.c
@@ -2215,6 +2215,58 @@ p_eor_register_shifted_register(uint32_t pc, uint32_t code)
     if (setflags) {
         emit_code("      APSR.N = !!(result & 0x80000000);");
         emit_code("      APSR.Z = (0 == result);");
+        // V unchanged
+    }
+    emit_code("      r%u = result;", Rd);
+    emit_code("    }");
+
+    pc_stack_push(pc + 4);
+}
+
+void
+p_and_register_shifted_register(uint32_t pc, uint32_t code)
+{
+    const uint32_t setflags = code & (1 << 20);
+    const uint32_t Rn = (code >> 16) & 0xf;
+    const uint32_t Rd = (code >> 12) & 0xf;
+    const uint32_t Rs = (code >> 8) & 0xf;
+    const uint32_t type = (code >> 5) & 0x3;
+    const uint32_t Rm = code & 0xf;
+    const enum SRType shift_t = arm_decode_imm_type(type, 0);
+
+    assert(Rn != 15);
+    assert(Rd != 15);
+    assert(Rs != 15);
+    assert(Rm != 15);
+
+    emit_code("    {");
+    emit_code("      const uint32_t qx = r%u;", Rn);
+    emit_code("      const uint32_t shift_n = r%u & 0xff;", Rs);
+    switch (shift_t) {
+    case SRType_LSL:
+        if (setflags)
+            emit_code("      if (shift_n > 0) APSR.C = !!(0x80000000 & (r%u << (shift_n - 1)));", Rm);
+        emit_code("      const uint32_t qy = r%u << shift_n;", Rm);
+        break;
+    case SRType_LSR:
+        if (setflags)
+            emit_code("      if (shift_n > 0) APSR.C = !!(0x1 & (r%u >> (shift_n - 1)));", Rm);
+        emit_code("      const uint32_t qy = r%u >> shift_n;", Rm);
+        break;
+    case SRType_ASR:
+        if (setflags)
+            emit_code("      if (shift_n > 0) APSR.C = !!(0x1 & ((int32_t)r%u >> (shift_n - 1)));", Rm);
+        emit_code("      const uint32_t qy = (int32_t)r%u >> shift_n;", Rm);
+        break;
+    default:
+        assert(0 && "shift type not implemented");
+    }
+
+    emit_code("      const uint32_t result = qx & qy;");
+    if (setflags) {
+        emit_code("      APSR.N = !!(result & 0x80000000);");
+        emit_code("      APSR.Z = (0 == result);");
+        // V unchanged
     }
     emit_code("      r%u = result;", Rd);
     emit_code("    }");
@@ -2397,6 +2449,8 @@ process_instruction(uint32_t pc)
         p_add_register_shifted_register(pc, code);
     } else if ((code & 0x0fe00090) == 0x00200010) {
         p_eor_register_shifted_register(pc, code);
+    } else if ((code & 0x0fe00090) == 0x00000010) {
+        p_and_register_shifted_register(pc, code);
     } else {
         printf("process_instruction(0x%04x, 0x%08x)\n", pc, code);
         assert(0 && "instruction code not implemented");
