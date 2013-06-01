263f933c506c633a49b5277265bd58e7ad6c546f
Rinat <ibragimovrinat@mail.ru>
2013-06-01 23:44:06 +0400

    move libom.so specific to target-specific.c

--- target-specific.h
+++ target-specific.h
@@ -1,6 +1,8 @@
 #pragma once
+#include <stdint.h>
 
 extern char so_md5_hash[33];
 
 void        compute_hash_of_so(const char *so_name);
 void        apply_target_specific_quirks(void);
+int         target_specific_symbols(const char *symname, uint32_t func_addr);
