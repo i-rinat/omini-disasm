cb1a3522faab33a463a07c49cf1d4139cacb8e8a
Rinat <ibragimovrinat@mail.ru>
2013-06-02 21:32:47 +0400

    emit_code_nonl

--- output.c
+++ output.c
@@ -20,6 +20,21 @@ emit_code(const char *fmt, ...)
     va_end(args);
 }
 
+/// @brief emit code to current output. Without new line character.
+void
+emit_code_nonl(const char *fmt, ...)
+{
+    FILE *target_fp = output_fp ? output_fp : stdout;
+    va_list args;
+
+    if (!output_fp)
+        fprintf(target_fp, "code emit:   ");
+    va_start(args, fmt);
+    vfprintf(target_fp, fmt, args);
+
+    va_end(args);
+}
+
 void
 set_output_file(const char *fname)
 {
--- output.h
+++ output.h
@@ -2,5 +2,6 @@
 
 
 void    emit_code(const char *fmt, ...);
+void    emit_code_nonl(const char *fmt, ...);
 void    set_output_file(const char *fname);
 void    close_output_file();
