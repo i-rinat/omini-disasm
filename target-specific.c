434578d76f59fb5eace545421d28c073eb46ab9e
Rinat <ibragimovrinat@mail.ru>
2013-06-03 01:12:00 +0400

    new function entries for libom.so

--- target-specific.c
+++ target-specific.c
@@ -139,7 +139,11 @@ apply_quirks_for_c3630424f7c9514b203301154218db40(void)
     func_list_mark_as_non_returning(0x7840);    // directly calls longjmp
     func_list_mark_as_non_returning(0x4da4);    // longjmp
 
+    func_list_add(0x43448);
     func_list_add(0x401f4);
+
+    func_list_add(0xffff0fc0);
+    func_list_mark_done(0xffff0fc0);
 
     uint32_t addr_table = 0x5CBC4;
     char *string_table = get_charptr_at(0x57888);
