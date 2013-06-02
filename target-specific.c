32ffb612b2bb970c06752a440c40b132a97cb4ca
Rinat <ibragimovrinat@mail.ru>
2013-06-02 11:54:31 +0400

    proxies for JNI

--- target-specific.c
+++ target-specific.c
@@ -20,6 +20,119 @@ compute_hash_of_so(const char *so_name)
 }
 
 static
+const char *
+signature_return_type(const char *signature)
+{
+    const char *ptr = strstr(signature, ")");
+    assert(ptr);
+    switch (*(ptr + 1)) {
+    case 'Z': // return "jboolean"; break;
+    case 'B': // return "jbyte"; break;
+    case 'C': // return "jchar"; break;
+    case 'S': // return "jshort"; break;
+    case 'F': // return "jfloat"; break;
+    case 'L': // return "jobject"; break;
+    case '[': // return "jobject"; break;
+    case 'I': return "uint32_t"; break;
+    case 'D': // return "jdouble"; break;
+    case 'J': return "uint64_t"; break;
+    case 'V': return "void"; break;
+    default:
+        assert(0 && "unknown type");
+    }
+}
+
+static
+int
+signature_get_param_count(const char *signature)
+{
+    const char *ptr = strstr(signature, "(");
+    assert(ptr);
+    ptr ++;
+    uint32_t param_count = 0;
+    while (1) {
+        switch (*ptr) {
+        case 'Z':
+        case 'B':
+        case 'C':
+        case 'S':
+        case 'F':
+        case 'I':
+        case 'D':
+        case 'J':
+        case 'V':
+            param_count ++; ptr ++; break;
+        case ')':
+            return param_count; break;
+        case 'L':
+            param_count ++;
+            while (*ptr != ';')
+                ptr ++;
+            ptr ++;
+            break;
+        case '[':
+            param_count ++;
+            ptr += 2;
+            break;
+        default:
+            assert(0);
+        }
+    }
+    assert(0);
+    return 0;
+}
+
+static
+const char *
+signature_get_param(const char *signature, int n)
+{
+    const char *ptr = strstr(signature, "(");
+    assert(ptr);
+    ptr ++;
+    uint32_t param_count = 0;
+    while (1) {
+        switch (*ptr) {
+        case 'D':
+        case 'J':
+            if (0 == n)
+                return "uint64_t";
+        case 'Z':
+        case 'B':
+        case 'C':
+        case 'S':
+        case 'F':
+        case 'I':
+        case 'V':
+            if (0 == n)
+                return "uint32_t";
+            param_count ++; ptr ++; break;
+        case ')':
+            assert(0); break;
+        case 'L':
+            if (0 == n)
+                return "uint32_t";
+            param_count ++;
+            while (*ptr != ';')
+                ptr ++;
+            ptr ++;
+            break;
+        case '[':
+            if (0 == n)
+                return "uint32_t";
+            param_count ++;
+            ptr += 2;
+            break;
+        default:
+            assert(0);
+        }
+        n = n - 1;
+    }
+    assert(0);
+    return 0;
+}
+
+
+static
 void
 apply_quirks_for_c3630424f7c9514b203301154218db40(void)
 {
@@ -39,11 +152,12 @@ apply_quirks_for_c3630424f7c9514b203301154218db40(void)
     char *method_name;
     char *signature;
 
+    uint32_t addr_table_offset = 0;
     while (1) {
         if (!*ptr)
             break;
         class_name = ptr;
-        printf("class: %s\n", class_name);
+        // printf("class: %s\n", class_name);
         ptr += strlen(ptr) + 1;
         while (1) {
             if (!*ptr)
@@ -55,8 +169,64 @@ apply_quirks_for_c3630424f7c9514b203301154218db40(void)
                 break;
             signature = ptr;
 
-            printf("    %s->%s, signature: %s\n", class_name, method_name, signature);
+            const uint32_t func_addr = get_word_at(addr_table + addr_table_offset);
+            // printf("    %s->%s, signature: %s\n", class_name, method_name, signature);
+            char buf[2048];
+            char *buf_ptr = &buf[0];
 
+            printf("%s ", signature);
+            buf_ptr += sprintf(buf_ptr, "static %s proxy_%04x(", signature_return_type(signature),
+                func_addr);
+            for (int k = 0; k < signature_get_param_count(signature); k ++) {
+                if (0 != k)
+                    buf_ptr += sprintf(buf_ptr, ", ");
+
+                buf_ptr += sprintf(buf_ptr, "%s param%d", signature_get_param(signature, k), k);
+            }
+            buf_ptr += sprintf(buf_ptr, ") {");
+
+            printf("%s\n", buf);
+            printf("    r13 = d_stack_start;\n");
+
+            buf_ptr = &buf[0];
+            uint32_t param_offset = 0;
+            for (int k = 0; k < signature_get_param_count(signature); k ++) {
+                if (!strcmp(signature_get_param(signature, k), "uint64_t")) {
+                    if (param_offset & 1)
+                        param_offset ++;
+                    if (param_offset < 4) {
+                        printf("    r%u = param%u;\n", param_offset, k);
+                        printf("    r%u = param%u >> 32;\n", param_offset + 1, k);
+                    } else {
+                        printf("    store(r13 + %u, param%u);\n", 4*(param_offset - 4), k);
+                        printf("    store(r13 + %u, param%u >> 32);\n", 4*(param_offset - 4) + 4, k);
+                    }
+
+                    param_offset += 2;
+                } else {
+                    if (param_offset < 4)
+                        printf("    r%u = param%u;\n", param_offset, k);
+                    else
+                        printf("    store(r13 + %u, param%u);\n", 4*(param_offset - 4), k);
+
+                    param_offset += 1;
+                }
+            }
+            printf("    func_%04x();\n", func_addr);
+
+            if (!strcmp(signature_return_type(signature), "void")) {
+                printf("    return;\n");
+            } else if (!strcmp(signature_return_type(signature), "uint32_t")) {
+                printf("    return r0;\n");
+            } else if (!strcmp(signature_return_type(signature), "uint64_t")) {
+                printf("    return reg.x_uint64_t;\n");
+            } else {
+                assert(0);
+            }
+
+            printf("}\n");
+
+            addr_table_offset += 4;
             ptr += strlen(ptr) + 1;
         }
         ptr ++;
